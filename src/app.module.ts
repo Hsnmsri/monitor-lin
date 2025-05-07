2a9bbe99fb64a36f4d9b7bb52f3fc227766acec1
Hossein Mansouri <hsnmsri82@gmail.com>
2025-05-07 21:34:26 +0330

    update project

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,11 @@ import { TelegramModule } from './core/modules/telegram/telegram.module';
 import { TelegramService } from './services/telegram/telegram.service';
 import { CpuService } from './services/cpu/cpu.service';
 import { MemoryService } from './services/memory/memory.service';
+import { DiskService } from './services/disk/disk.service';
 
 @Module({
   imports: [TelegramModule],
   controllers: [AppController],
-  providers: [AppService, TelegramService, CpuService, MemoryService],
+  providers: [AppService, TelegramService, CpuService, MemoryService, DiskService],
 })
 export class AppModule { }
